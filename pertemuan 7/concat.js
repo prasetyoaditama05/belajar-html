a6ac60d37803bdc515782bdbb20f38839dc08fdf
PrasetyoAditama <prasetyoaditama050405@gmail.com>
2025-05-14 23:53:04 +0700

    new file:   pertemuan 7/concat.js 	new file:   pertemuan 7/persegipanjang.js 	new file:   pertemuan 7/tabung.js

---
+++ pertemuan 7/concat.js
@@ -0,0 +1,6 @@
+let firstName = "John";
+let LastName = "Doe";
+let bornYear = 1998;
+console.log("hello, " + firstName + " " + LastName + " ! You are " + (2014-bornYear) + " years old" );
+
+
---
+++ pertemuan 7/persegipanjang.js
@@ -0,0 +1,8 @@
+let panjang = 20;
+let lebar = 4; 
+
+let luas = panjang * lebar
+console.log(luas);
+
+let keliling = 2 * (panjang + lebar)
+console.log(keliling);
